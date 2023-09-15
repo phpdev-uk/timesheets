cc9b4704402a2181a2ad8cb17291b38e5361cfbc
Paul Waring <paul@xk7.net>
2023-09-15 09:31:03 +0100

    First draft of client subcommand

---
+++ src/ReadFilter.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpdevUk\Timesheets;
+
+use PhpOffice\PhpSpreadsheet\Reader\IReadFilter;
+
+class ReadFilter implements IReadFilter
+{
+    protected string $startColumn;
+    protected string $endColumn;
+
+    public function __construct(string $startColumn, string $endColumn)
+    {
+        $this->startColumn = $startColumn;
+        $this->endColumn = $endColumn;
+    }
+
+    public function readCell($columnAddress, $row, $worksheetName = '')
+    {
+        return in_array($columnAddress, range($this->startColumn, $this->endColumn));
+    }
+}
---
+++ timesheets.php
@@ -0,0 +1,117 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpdevUk\Timesheets\ReadFilter;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+const DATE_TIME_FORMAT = 'd/m/Y H:i:s A';
+
+$application = new Application();
+
+$application
+    ->register('client')
+    ->addArgument(
+        'client',
+        InputArgument::REQUIRED
+    )
+    ->addArgument(
+        'files',
+        InputArgument::IS_ARRAY | InputArgument::REQUIRED
+    )
+    ->addOption(
+        'start-date',
+        'start',
+        InputOption::VALUE_REQUIRED,
+        'Start Date (inclusive)'
+    )
+    ->addOption(
+        'end-date',
+        'end',
+        InputOption::VALUE_REQUIRED,
+        'End Date (inclusive)'
+    )
+    ->setCode(function (InputInterface $input, OutputInterface $output): int {
+        $clientNameFilter = $input->getArgument('client');
+        $files = $input->getArgument('files');
+
+        $workChunks = [];
+
+        foreach ($files as $file) {
+            if (!file_exists($file)) {
+                $output->writeln('File does not exist: ' . $file);
+                return Command::FAILURE;
+            }
+
+            if (!is_file($file)) {
+                $output->writeln('Not a file: ' . $file);
+                return Command::FAILURE;
+            }
+
+            $readFilter = new ReadFilter('A', 'E');
+
+            $reader = IOFactory::createReaderForFile($file);
+            $reader->setReadDataOnly(false); // This must be false for formatted data to be returned
+            $reader->setReadEmptyCells(false); // We only need non-empty cells, can guarantee each row will be populated
+            $reader->setReadFilter($readFilter);
+            $spreadsheet = $reader->load($file);
+            $sheetCount = $spreadsheet->getSheetCount();
+
+            for ($s = 0; $s < $sheetCount; $s++) {
+                $sheet = $spreadsheet->getSheet($s);
+
+                if ($output->isVerbose()) {
+                    $output->writeln($sheet->getTitle());
+                }
+
+                $highestRow = $sheet->getHighestDataRow();
+
+                for ($row = 2; $row <= $highestRow; $row++) {
+                    $clientName = $sheet->getCell("E{$row}")->getFormattedValue();
+
+                    if ($clientName == $clientNameFilter) {
+                        $date = $sheet->getCell("A{$row}")->getFormattedValue();
+                        $startTime = $sheet->getCell("B{$row}")->getFormattedValue();
+                        $endTime = $sheet->getCell("C{$row}")->getFormattedValue();
+
+                        $startDateTime = DateTimeImmutable::createFromFormat(DATE_TIME_FORMAT, "$date $startTime");
+                        $endDateTime = DateTimeImmutable::createFromFormat(DATE_TIME_FORMAT, "$date $endTime");
+
+                        $diff = $startDateTime->diff($endDateTime);
+
+                        $workChunks[] = [
+                            'hours' => $diff->h,
+                            'minutes' => $diff->i,
+                        ];
+
+                        if ($output->isVerbose()) {
+                            $output->writeln("{$diff->h}h{$diff->i}m");
+                        }
+                    }
+                }
+            }
+        }
+
+        $totalHours = array_sum(array_column($workChunks, 'hours'));
+        $totalMinutes = array_sum(array_column($workChunks, 'minutes'));
+
+        while ($totalMinutes >= 60) {
+            $totalHours++;
+            $totalMinutes -= 60;
+        }
+
+        $output->writeln("Total hours: $totalHours");
+        $output->writeln("Total minutes: $totalMinutes");
+
+        return Command::SUCCESS;
+    });
+
+$application->run();
